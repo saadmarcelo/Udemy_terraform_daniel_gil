6629fb2d5fe2de9f6ae99b2e505ff93a153ced83
Marcelo saad <marcelo_saad@hotmail.com>
2025-01-21 14:38:54 -0400

    blocos de estrutura

---
+++ estrutura-blocos/main.tf
@@ -0,0 +1,30 @@
+terraform {
+
+}
+
+provider "aws" {
+
+}
+
+resource "aws_instance" "nome" {
+
+}
+
+data "aws_ami" "image" {
+
+}
+
+module "vpc" {
+
+}
+variable "vm_name" {
+
+}
+
+output "vm1_ip" {
+
+}
+
+locals {
+  
+}
