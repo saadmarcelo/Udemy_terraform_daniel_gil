ab8ca51f11b30ac42518178c4351bcac500abedd
Marcelo saad <marcelo_saad@hotmail.com>
2025-01-23 14:55:04 -0400

    aws-vpc and remote state

---
+++ aws-vpc/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "marcelosaad-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      owner      = "MarceloSaad"
+      managed-by = "Terraform"
+    }
+  }
+}
